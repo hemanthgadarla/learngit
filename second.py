4288e24b20cb0a4b2a9bd679998fab1a3b03d13a
hemanth <hemanth.gadarla@zemosolabs.com>
2021-04-11 15:22:12 +0530

    primes program

--- second.py
+++ second.py
@@ -1,4 +1,26 @@
-print("this is another file created");
-print("this is another edit in staging process");
-print("data in dev branch");
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
 
